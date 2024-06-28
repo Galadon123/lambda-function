0fb4ea3115cd3ef74e508096007349384acd188e
Fazlul Karim <fazlulkarim362@gmail.com>
2024-06-28 17:22:06 +0600

    udaf

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-export const handler = async (event) => {
-    // TODO implement elem
+exports.handler = async (event) => {
+    console.log("Event: ", event);
     const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!'),
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda vsdfgsfgsvsgf!'),
     };
     return response;
-  };
-  
+};
