a7a8cc24f16d8f8904ce7aba11bdbfee6a3e558d
Fazlul Karim <fazlulkarim362@gmail.com>
2024-06-28 17:30:03 +0600

    commit-2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     console.log("Event: ", event);
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda '),
+        body: JSON.stringify('hello world \n the lambda is updated! '),
     };
     return response;
 };
